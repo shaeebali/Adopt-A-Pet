9046e01b29f8095752af4a934f207fd082c238ce
shaeeb <shaeebali@gmail.com>
2023-07-05 12:00:56 +0100

    add cat/dog routes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } 
 const appRouter = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={ <Root/> }>
     <Route index element={<HomePage />} />
-    
+    <Route path=":type" element={<HomePage />} />
   </Route>
 ));
 
